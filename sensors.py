8973ea5ab42fbc05657ea7f9e9edd5f5e69e1bba
TimShier <tim.shier@gmail.com>
2014-02-16 20:18:04 +0200

    Beginning of sensor scanning

--- sensors.py
+++ sensors.py
@@ -1,6 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
 class Sensor:
+
+	def lookSee():
+		try:
+			while True:
+				GPIO.output(7,1)
+				time.sleep(0.0015)
+				GPIO.output(7,0)
+				time.sleep(2)
+		except KeyboardInterrupt:
+			GPIO.cleanup()
+	
 	def __init__(self):
 		# init.
 		print ("Sensor up!")
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(7,GPIO.OUT)
+		lookSee()
 
 
